7e583799f98696d48da32f0c8ccd271a796393a8
ShadowPower <617274873@qq.com>
2017-01-15 11:03:46 +0800

    移除instantclick

--- OJ/static/js/oj.js
+++ OJ/static/js/oj.js
@@ -1,13 +1,5 @@
 var oj = (function () {
 
-    InstantClick.on('change', function(isInitialLoad) {
-      if (isInitialLoad === false) {
-
-      }
-    });
-
-    InstantClick.init('50');
-
     // 导出函数
     return {
     }
