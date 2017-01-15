c92e57d5eb3576ce0f7485ae676a5c1420fd27a7
ShadowPower <617274873@qq.com>
2017-01-15 10:44:59 +0800

    删除无用JS，添加instantclick

--- OJ/static/js/oj.js
+++ OJ/static/js/oj.js
@@ -1,19 +1,5 @@
 var oj = (function () {
-    // 根据时间更改背景配色
-    function changeBackgroundByTime () {
-        var background = document.getElementById('background');
-        if (background == null)
-            return;
-
-        var currentTime = new Date();
-        var hours = currentTime.getHours();
-        if (hours >= 19 || hours < 7)
-            background.setAttribute('class', 'background night');
-        else if (hours >= 7 && hours < 17)
-            background.setAttribute('class', 'background day');
-        else
-            background.setAttribute('class', 'background dusk');
-    }
+    InstantClick.init(50);
 
     // 导出函数
     return {
