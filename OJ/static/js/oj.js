83752772d73000e851fa669277c621459d710327
ShadowPower <617274873@qq.com>
2017-02-10 18:23:51 +0800

    改回切换分页Modal

--- OJ/static/js/oj.js
+++ OJ/static/js/oj.js
@@ -1,4 +1,13 @@
 var oj = (function () {
+    window.onload = function () {
+        $('#change-pagination').on('click', function (e) {
+            e.preventDefault();
+            $(this).blur();
+            UIkit.modal.prompt('请输入页码 (7/20):', '').then(function(page) {
+                console.log('跳转到:', page)
+            });
+        });
+    }
 
     // 导出函数
     return {
