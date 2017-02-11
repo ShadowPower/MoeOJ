867a980f27caa148fa10c2435fb3764ea6e1efaa
ShadowPower <617274873@qq.com>
2017-02-11 21:35:02 +0800

    多处修改 修改问题列表页面，添加“来源” 更改时区为“上海” 建立比赛页面的基本模板并创建各页面 建立比赛页面URL和View 完善比赛页面基本模板的Tab Tab作为超链接使用

--- OJ/views.py
+++ OJ/views.py
@@ -30,6 +30,39 @@ def contest_overview(request, contest_id):
     overview_text = "###欢迎参加XXX大赛\n本次大赛准备了丰厚的奖品  \n第一名可免费使用一年校园网\n\n祝大家比赛愉快~喵~\n\n<del>第二名可以免费用两年！</del>  \n![头像](https://avatars1.githubusercontent.com/u/7625230?s=100)"
     return render(request, "contest/contest-overview.html", {
         'background_class': Helper.get_background_class(),
+        'contest_id': contest_id,
         'contest_title': contest_title,
         'overview_text': markdown(overview_text)
+    })
+
+def contest_problemset(request, contest_id):
+    contest_title = "比赛标题"
+    return render(request, "contest/contest-problemset.html", {
+        'background_class': Helper.get_background_class(),
+        'contest_id': contest_id,
+        'contest_title': contest_title,
+    })
+
+def contest_ranklist(request, contest_id):
+    contest_title = "比赛标题"
+    return render(request, "contest/contest-ranklist.html", {
+        'background_class': Helper.get_background_class(),
+        'contest_id': contest_id,
+        'contest_title': contest_title,
+    })
+
+def contest_statistics(request, contest_id):
+    contest_title = "比赛标题"
+    return render(request, "contest/contest-statistics.html", {
+        'background_class': Helper.get_background_class(),
+        'contest_id': contest_id,
+        'contest_title': contest_title,
+    })
+
+def contest_status(request, contest_id):
+    contest_title = "比赛标题"
+    return render(request, "contest/contest-status.html", {
+        'background_class': Helper.get_background_class(),
+        'contest_id': contest_id,
+        'contest_title': contest_title,
     })
