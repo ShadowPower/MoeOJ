ae7465efd6efdb9c190ae0d4661e5b7c1a61a32d
ShadowPower <617274873@qq.com>
2017-02-20 17:26:44 +0800

    修改Footer字体颜色和Allowed hosts

--- MoeOJ/settings.py
+++ MoeOJ/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!$-8fa^%z3_+xyzm7oe(^r!hbzc2%8u!myys+vzw^vt@()eu&o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
